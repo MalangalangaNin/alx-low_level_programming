c70e87c0ca444168676e48074abd2b79193d9325
Neyner <overrpaid@gmail.com>
2022-03-15 12:04:25 -0700

    true

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,43 +1,22 @@
 #include "holberton.h"
 
 /**
+ *main - chec the code 
  *
- *  * main - check the code.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
-void print_alphabet_x10(void)
-
+ *Return always zero 
+ */
+int main (void)
 {
+ int i ;
+ char c ;
 
-	    int i;
-
-	        char c;
-
-		    
-
-		    for (i = 0; i < 10; ++i)
-
-			        {
-
-					        for (c = 'a'; c <= 'z'; c++)
-
-							        {
-
-									            _putchar(c);
-
-										            }
-
-						        _putchar('\n');
-
-							     }
-
-		            return (0);
-
+for (i=0; i<10; ++i;)
+{
+for (c='a' ; c<='z' ; c++ )
+{
+_putchar(c);
 }
-
-
+_putchar('\n');
+}
+return (0);
+}
